bd39568acd28560ae42f29750e996382bd93de42
Ali <ali.salim.serikuly@gmail.com>
2024-01-25 11:51:31 +0600

    Added modal menu

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,6 +1,9 @@
 const hamburger = document.querySelector('.hamburger'),
       menu = document.querySelector('.menu'),
-      closeMenu = document.querySelector('.menu__close');
+      closeMenu = document.querySelector('.menu__close'),
+      loginButton = document.querySelector('.header__btn'),
+      modal = document.querySelector('.overlay'),
+      modalClose = document.querySelector('.modal__close');
 
 
 hamburger.addEventListener('click', (e) => {
@@ -9,4 +12,12 @@ hamburger.addEventListener('click', (e) => {
 
 closeMenu.addEventListener('click', (e) => {
     menu.classList.remove('menu_active')
+})
+
+loginButton.addEventListener('click', (e) => {
+    modal.classList.add('overlay_active')
+})
+
+modalClose.addEventListener('click', (e) => {
+    modal.classList.remove('overlay_active')
 })
